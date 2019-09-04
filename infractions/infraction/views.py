d53035bae06ba002671685c877df2c6d9052b05a
Aron Van der Haegen <atomicprobomb@gmail.com>
2019-09-04 14:48:38 +0200

    Minimum infraction filtering

--- infractions/infraction/views.py
+++ infractions/infraction/views.py
@@ -4,6 +4,23 @@ from django.http import JsonResponse
 import json
 # Create your views here.
 
+def infractionsMin(request, min):
+    with open('./infractions.json') as f:
+        infractions_dict = json.load(f)
+    
+    json_data = open('./infractions.json')   
+    data1 = json.load(json_data) # deserialises it
+    data2 = json.dumps(data1, indent=4, sort_keys=True) # json formatted string
+    output = "<body>"
+
+    infractions_dict.sort(key=extract_time)
+    for infraction in infractions_dict:
+        if infraction['infractions_speed'] >= int(min):
+            output += "<p>" + str(infraction) + "</p><br>"
+    output += "</body>"
+    return HttpResponse(output)
+    json_data.close()
+
 def infractions(request):
     with open('./infractions.json') as f:
         infractions_dict = json.load(f)
