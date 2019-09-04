181cf6d3059ed803fc5435e4bdd0b13bda6a675f
Aron Van der Haegen <atomicprobomb@gmail.com>
2019-09-04 13:24:54 +0200

    Setup Infractions page

--- infractions/infraction/views.py
+++ infractions/infraction/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def infractions(request):
+    return HttpResponse("Don't do me like this.")
